67240f121dfe9e0e0b9ed1fcaddde12a36e3f422
JeffreyUchicago <jiangyuzhao@uchicago.edu>
2019-04-04 16:16:12 -0500

    Update spyder prac.py

--- spyder prac.py
+++ spyder prac.py
@@ -7,4 +7,7 @@ This is a temporary script file.
 for i in range(10):
     print(i)
 
- 
+# Now let's generate a random number between 1 to 6
+import numpy as np
+x = np.random.randint(1, 7)
+print(x)
