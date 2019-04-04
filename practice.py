f914d851268c0da905620b79edddcd5c805395bd
JeffreyUchicago <jiangyuzhao@uchicago.edu>
2019-04-04 15:41:20 -0500

    Some corrections

--- practice.py
+++ practice.py
@@ -1,9 +1,10 @@
 # Intro to Programming Pre-class Exercises
 # Editor: Jiangyu Zhao, Apr. 4th
 
-for num in range():
+for num in range(10):
     print(i)
 
 # Now try to add more comments
 import numpy as np
 x = np.random.rand()
+print(x)
